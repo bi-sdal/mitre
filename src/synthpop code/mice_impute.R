c44284ae741f66f79a1f50c0d99da3fa26771622
Ian Crandell <ian85@vbi.vt.edu>
2018-01-16 17:06:23 +0000

    Created project folder for MITRE. Added Josh's synthetic population code.

---
+++ src/synthpop code/mice_impute.R
@@ -0,0 +1,232 @@
+# impute HINCP, VALP, TAXP2 using mice for conditional draws, and a distribution for marginal draws
+# marginal distribution: uniform with exponential tails
+# problem: zero counts in bins are treated as zero likelihood, but are due to small ACS sample size
+#   I implement a 'zero inflated' uniform; e.g. z/2% of income probability is a uniform draw anywhere from (0,2e5)
+# second problem: zero counts in 200,000+ bin also treated as zero likelihood; inflate exponential tail by z/2%
+
+library(dplyr)
+library(mice)
+
+setwd("/home/sdal/projects/hud_census/analysis/Josh/synthetic population/person variables/")
+load("cleaned_CL_ACS.RData")
+
+# -----------------------------------------------------------------------
+# threshhold CoreLogic data to maximum in PUMS
+# -----------------------------------------------------------------------
+
+max(PUMS$VALP) # $1.4m
+max(PUMS$TAXP2) # $33,368
+
+CLdata$TOTAL.VALUE.CALCULATED[CLdata$TOTAL.VALUE.CALCULATED > max(PUMS$VALP)] <- max(PUMS$VALP)
+CLdata$TAX.AMOUNT[CLdata$TAX.AMOUNT > max(PUMS$TAXP2)] <- max(PUMS$TAXP2)
+
+# -----------------------------------------------------------------------
+# generate 1000 conditional samples with mice
+# -----------------------------------------------------------------------
+
+# create combined data frame
+CLdata$source <- "CL"
+CLdata$HINCP <- NA
+PUMS$source <- "PUMS"
+PUMS$BlockGroup <- NA
+CL_PUMS <- rbind( CLdata %>% select(HINCP,VALP=TOTAL.VALUE.CALCULATED,TAXP2=TAX.AMOUNT,BlockGroup,source),
+                  PUMS %>% select(HINCP,VALP,TAXP2,BlockGroup,source) )
+# transform income to satisfy linear regression assumptions
+CL_PUMS$sqrtHINCP <- sqrt(CL_PUMS$HINCP)
+
+# impute using Bayesian linear regression; add the imputed values to data frame
+# be careful of memory constraints on number of draws (100 draws is 360Mb)
+mice.out <- mice(data=CL_PUMS%>%select(sqrtHINCP,VALP,TAXP2), m=100, method="norm")
+imputed_draws <- mice.out$imp$sqrtHINCP[,1:100]
+for(i in 2:10){ #1000 total draws
+  mice.out <- mice(data=CL_PUMS%>%select(sqrtHINCP,VALP,TAXP2), m=100, method="norm")
+  imputed_draws <- cbind(imputed_draws,mice.out$imp$sqrtHINCP[,1:100])
+}
+imputed_draws[imputed_draws < 0] <- 0 # restrict household income to be positive
+
+# -----------------------------------------------------------------------
+# get the marginal distribution for income by block group; piecewise uniform with exponential tails
+# -----------------------------------------------------------------------
+
+# use counts from ACS table to estmiate a piecewise uniform distribution by blockgroup
+# fit an exponential tail when income is $200,000+
+income_marginal <- read.csv("ACS_13_5YR_B19001_with_ann.csv",header=TRUE,skip=1)[,c(1:3,seq(6,37,by=2))]
+income_marginal$BlockGroup <-  as.numeric(substr(income_marginal$Id2,6,12))
+income_marginal <- cbind(BlockGroup=income_marginal$BlockGroup,income_marginal[,4:19])
+income_marginal <- income_marginal %>% filter(BlockGroup != 9801001) # filter out blockgroup with 0 observations
+# aggregate counts every 25k
+income_marginal <- income_marginal %>% transmute(BlockGroup,"0-25k"=Estimate..Total.....10.000.to..14.999+Estimate..Total....Less.than..10.000+
+                                                       Estimate..Total.....15.000.to..19.999+Estimate..Total.....20.000.to..24.999,
+                                                     "25-50k"=Estimate..Total.....25.000.to..29.999+Estimate..Total.....30.000.to..34.999+
+                                                       Estimate..Total.....35.000.to..39.999+Estimate..Total.....40.000.to..44.999+Estimate..Total.....45.000.to..49.999,
+                                                     "50-75k"=Estimate..Total.....50.000.to..59.999+Estimate..Total.....60.000.to..74.999,
+                                                     "75-100k"=Estimate..Total.....75.000.to..99.999,
+                                                     "100-125k"=Estimate..Total.....100.000.to..124.999,
+                                                     "125-150k"=Estimate..Total.....125.000.to..149.999,
+                                                     "150-200k"=Estimate..Total.....150.000.to..199.999,
+                                                     "200+k"=Estimate..Total.....200.000.or.more)
+#breaks <- c(-1,10000,15000,20000,25000,30000,35000,40000,45000,50000,60000,75000,100000,125000,150000,200000)
+breaks <- c(-1,25000,50000,75000,100000,125000,150000,200000)
+
+ncols <- ncol(income_marginal)
+z <- 0.1 # zero inflated; uniform from 0 to 2e5 by z/2, exponential tails by z/2 to capture zero counts in the ACS
+
+# area under piecewise uniform (histogram) density: percent of observations under 200,000
+area_hist <- (1-z/2)*rowSums(income_marginal[,2:(ncols-1)])/rowSums(income_marginal[,2:ncols])
+# area under exponential tail: percent over 200,000
+area_exp <- (1-z/2)*income_marginal[,ncols]/rowSums(income_marginal[,2:ncols]) + z/2
+
+
+widths <- diff(breaks)
+marginal_uniform_density <- (1-z/2)*income_marginal[,2:(ncols-1)]/rowSums(income_marginal[,2:ncols])
+marginal_uniform_density <- sweep(marginal_uniform_density,2,z/2*widths/sum(widths),FUN = "+") # add constant uniform
+marginal_uniform_density_mat <- as.data.frame((as.matrix(marginal_uniform_density) %*% diag(1/widths)))
+names(marginal_uniform_density_mat) <- names(marginal_uniform_density)
+marginal_uniform_density_mat <- cbind(BlockGroup=income_marginal$BlockGroup,marginal_uniform_density_mat)
+
+# next, fit exponential tail for values > 200,000
+samp_income <- PUMS$HINCP[PUMS$HINCP > 200000 & !is.na(PUMS$HINCP)] - 200000
+# use the MLE for an exponential distribution, 1/x-bar
+mle_lambda <- 1/mean(samp_income)
+# plot the fitted exponential tail
+hist(samp_income+2e5, freq=FALSE,breaks=30,xlab="Income",main="PUMS Fitted Exponential Tail")
+xs <- seq(2e5,8e5,length=100)
+lines(xs, mle_lambda*exp(-mle_lambda*(xs-2e5)),col=2,lwd=2)
+
+# write a function for the marginal distribution for each blockgroup; make plots
+# vectorize this function (vector of inputs for income and blockgroup)
+marginal_dist <- function(income,blockgroup){
+  # get the piecewise uniform density, area_exp corresponding to this blockgroup
+  ind <- match(blockgroup,marginal_uniform_density_mat$BlockGroup)
+  uniform_density_bg <- as.matrix(marginal_uniform_density_mat[ind,-1])
+  area_exp_bg <- area_exp[ind]
+  
+  density_out <- rep(NA,length(income))
+  # find the range income falls into, return uniform density
+  cut_ind <- cut(income,breaks,labels=1:(length(breaks)-1))
+  for(i in 1:length(density_out)){
+    density_out[i] <- uniform_density_bg[i,cut_ind[i]]
+  }
+  # restricted to positive income
+  density_out[income < 0] <- 0
+  # exponential tail
+  ind_highincome <- which(income >= 2e5)
+  density_out[ind_highincome] <- area_exp_bg[ind_highincome] * mle_lambda * exp(-mle_lambda*(income[ind_highincome]-2e5))
+  
+  return(density_out)
+}
+
+#marginal_dist(140000,1002002)
+#test <- marginal_dist(income=runif(n=nrow(CLdata),min=0,max=4e5),blockgroup=CLdata$BlockGroup)
+#sum(log(test)) # loglikelihood
+
+# -----------------------------------------------------------------------
+# plot the marginal distributions by blockgroup for a sample of 20
+# -----------------------------------------------------------------------
+
+par(mfrow=c(5,4))
+par(mar=c(3,3,1,1))
+samp_bg <- sort(sample(marginal_uniform_density_mat$BlockGroup,20))
+for(i in 1:20){
+  xs <- seq(0,4e5,length=400)
+  bg <- samp_bg[i]
+  ys <- marginal_dist(xs,rep(bg,length(xs)))
+  plot(xs,ys,type="l",xlab="Income",ylab="Density",main="",ylim=c(0,max(ys)))
+  text(paste(bg),x=3.3e5,y=max(ys)*.75,font=2,cex=1.5)
+}
+
+# -----------------------------------------------------------------------
+# draw imputed samples from the marginal distribution (independantly for each household)
+# -----------------------------------------------------------------------
+
+ndraws <- 20
+
+imputed_income <- imputed_draws^2
+marginal_weights <- matrix(NA,nrow=nrow(imputed_draws),ncol=ncol(imputed_draws))
+marginal_samp_prob <- matrix(NA,nrow=nrow(imputed_draws),ncol=ncol(imputed_draws))
+
+for(i in 1:ncol(imputed_draws)){
+  marginal_weights[,i] <- marginal_dist(income=imputed_income[,i],blockgroup=CLdata$BlockGroup) # likelihoods
+  marginal_samp_prob[,i] <- marginal_weights[,i]/sum(marginal_weights[,i])
+  if(i%%100==0){print(i)}
+}
+
+#sort(marginal_samp_prob[,1],decreasing = T)[1:10]
+#sort(marginal_samp_prob[,1],decreasing = F)[1:10]
+#max(marginal_samp_prob[,1])/min(marginal_samp_prob[,1])
+
+# draw samples from weighted marginal distribution
+income_draws <- matrix(NA,nrow=nrow(imputed_income),ncol=ndraws)
+for(i in 1:nrow(imputed_income)){
+  income_draws[i,] <- sample(size=ndraws, x=imputed_income[i,], prob=marginal_samp_prob[i,], replace=TRUE)
+}
+
+# -----------------------------------------------------------------------
+# plot imputed joint income for a single draw by blockgroup (histogram) vs marginal distributions
+# -----------------------------------------------------------------------
+
+income_draws_plot <- as.data.frame(cbind(income=income_draws[,1],BlockGroup=CLdata$BlockGroup))
+
+png("marginal_with_joint_imputed.png",width=800,height=600)
+par(mfrow=c(5,4))
+par(mar=c(3,3,1,1))
+samp_bg <- sort(sample(unique(income_draws_plot$BlockGroup),20))
+for(i in 1:20){
+  xs <- seq(0,4e5,length=400)
+  bg <- samp_bg[i]
+  ys <- marginal_dist(xs,rep(bg,length(xs)))
+  ymax <- max( c(ys,hist( (income_draws_plot %>% filter(BlockGroup==bg))[,1], plot=FALSE )$density ) )
+  hist( (income_draws_plot %>% filter(BlockGroup==bg))[,1], add=FALSE, freq = FALSE, xlim=c(0,4e5),ylim=c(0,ymax*1.1),
+        col="lightblue",border="blue",main="",xlab="Income",ylab="Density",
+        breaks = c(breaks,2.5e5,3e5,3.5e5,4e5,Inf) )
+  lines(xs,ys,type="l",xlab="Income",ylab="Density",main="",lwd=2,col="red")
+  text(paste(bg),x=3.3e5,y=max(ys)*.75,font=2,cex=1.5)
+}
+dev.off()
+
+# -----------------------------------------------------------------------
+# plot imputed conditional income (from mice) for a single draw by blockgroup (histogram) vs marginal distributions
+# -----------------------------------------------------------------------
+
+income_conditional_plot <- as.data.frame(cbind(income=imputed_draws[,1]^2,BlockGroup=CLdata$BlockGroup))
+
+png("marginal_with_mice_draw.png",width=800,height=600)
+par(mfrow=c(5,4))
+par(mar=c(3,3,1,1))
+#samp_bg <- sort(sample(unique(income_draws_plot$BlockGroup),20))
+for(i in 1:20){
+  xs <- seq(0,4e5,length=400)
+  bg <- samp_bg[i]
+  ys <- marginal_dist(xs,rep(bg,length(xs)))
+  ymax <- max( c(ys,hist( (income_draws_plot %>% filter(BlockGroup==bg))[,1], plot=FALSE )$density ) )
+  hist( (income_conditional_plot %>% filter(BlockGroup==bg))[,1], add=FALSE, freq = FALSE, xlim=c(0,4e5),ylim=c(0,ymax*1.1),
+        col="green",border="darkgreen",main="",xlab="Income",ylab="Density",
+        breaks = c(breaks,2.5e5,3e5,3.5e5,4e5,Inf) )
+  lines(xs,ys,type="l",xlab="Income",ylab="Density",main="",lwd=2,col="red")
+  text(paste(bg),x=3.3e5,y=max(ys)*.75,font=2,cex=1.5)
+}
+dev.off()
+
+
+# -----------------------------------------------------------------------
+# plot joint distributions of imputed income, value, taxes vs PUMS
+# -----------------------------------------------------------------------
+
+png("conditional_imputed.png",width=800,height=800)
+CL_PUMS_plot <- CL_PUMS
+CL_PUMS_plot$HINCP[CL_PUMS_plot$source=="CL"] <- income_draws[,1]
+CL_PUMS_plot$color <- "black"
+CL_PUMS_plot$color[CL_PUMS_plot$source=="CL"] <- "red"
+CL_PUMS_plot$VALP[CL_PUMS_plot$VALP < max(CL_PUMS_plot$VALP)] <- pmin( rnorm(length(CL_PUMS_plot$VALP[CL_PUMS_plot$VALP < max(CL_PUMS_plot$VALP)]),
+                                                                       mean=CL_PUMS_plot$VALP[CL_PUMS_plot$VALP < max(CL_PUMS_plot$VALP)],
+                                                                       sd=CL_PUMS_plot$VALP/8), 1400000 )
+
+par(xpd=FALSE)
+pairs(~HINCP+VALP+TAXP2,data=CL_PUMS_plot,col=CL_PUMS_plot$color,pch=20,cex=0.5)
+par(xpd=TRUE)
+legend(x=0,y=1, legend=c("PUMS","CoreLogic (Imputed HINCP)"), fill=c("black","red"),cex=1.5)
+dev.off()
+
+
+
+
---
+++ src/synthpop code/prepare_data.R
@@ -0,0 +1,155 @@
+# files:
+# CoreLogic housing data: Arlington_CL_2013_Data.csv
+# PUMS household microdata: ss14hva.csv
+# published ACS table for household income by block group:
+#     ACS_13_5YR_B19001_with_ann.csv
+
+# CoreLogic housing data is our universe for Arlington.
+# It has measurements of property value and taxes paid; we will impute household income.
+
+# method:
+# Step 1: Impute household income onto our synthetic universe using samples from the PUMS.
+# Repeat Step 1 to get many realizations of the synthetic universe.
+# Step 2: For each household, resample imputed realizations weighted by the marginal distribution of household income by block group.
+
+library(dplyr)
+library(MASS)
+
+setwd("~/sdal/projects/hud/hud_census/analysis/Josh/synthetic population/person variables/")
+
+# read in CoreLogic data for Arlington; subset by single family and by variables of interest
+# including home value (TOTAL.VALUE.CALCULATED), taxes paid (TAX.AMOUNT)
+CLdata<-read.csv("~/sdal/projects/hud/hud_census/data/CoreLogic/3_TIDY/Arlington_CL_2013_Data.csv") %>%
+  filter(PropertyType!="Multifamily",!is.na(TOTAL.VALUE.CALCULATED),!is.na(TAX.AMOUNT),!is.na(BlockGroup_rec)) %>%
+  dplyr::select(BlockGroup=BlockGroup_rec,TOTAL.VALUE.CALCULATED,TAX.AMOUNT,LATITUDE,LONGITUDE) %>%
+  arrange(BlockGroup)
+
+# read in ACS pums for Arlington (two PUMAs)
+# subset single family and by variables of interest
+# including household income (HINCP), home value (VALP), taxes paid (TAXP)
+PUMS <- read.csv("~/sdal/projects/hud/hud_census/analysis/Josh/synthetic population/PUMS/ss14hva.csv") %>%
+  dplyr::select(PUMA10,HINCP,VALP,TAXP) %>%
+  filter(PUMA10 %in% c(1301,1302), !is.na(TAXP),!is.na(VALP),!is.na(HINCP), VALP < 1500000, HINCP >= 0)
+
+# ------------------------------------------------------------------------------------------------
+# TAXP is binned; to use it as a predictor in our model, convert it into $$ by taking the center value of each bin
+# ------------------------------------------------------------------------------------------------
+
+PUMS$TAXP2[PUMS$TAXP==1] <- 0
+PUMS$TAXP2[PUMS$TAXP==2] <- 25
+PUMS$TAXP2[PUMS$TAXP==3] <- 75
+PUMS$TAXP2[PUMS$TAXP==4] <- 125
+PUMS$TAXP2[PUMS$TAXP==5] <- 175
+PUMS$TAXP2[PUMS$TAXP==6] <- 225
+PUMS$TAXP2[PUMS$TAXP==7] <- 275
+PUMS$TAXP2[PUMS$TAXP==8] <- 325
+PUMS$TAXP2[PUMS$TAXP==9] <- 375
+PUMS$TAXP2[PUMS$TAXP==10] <- 425
+PUMS$TAXP2[PUMS$TAXP==11] <- 475
+PUMS$TAXP2[PUMS$TAXP==12] <- 525
+PUMS$TAXP2[PUMS$TAXP==13] <- 575
+PUMS$TAXP2[PUMS$TAXP==14] <- 625
+PUMS$TAXP2[PUMS$TAXP==15] <- 675
+PUMS$TAXP2[PUMS$TAXP==16] <- 725
+PUMS$TAXP2[PUMS$TAXP==17] <- 775
+PUMS$TAXP2[PUMS$TAXP==18] <- 825
+PUMS$TAXP2[PUMS$TAXP==19] <- 875
+PUMS$TAXP2[PUMS$TAXP==20] <- 925
+PUMS$TAXP2[PUMS$TAXP==21] <- 975
+PUMS$TAXP2[PUMS$TAXP==22] <- 1050
+PUMS$TAXP2[PUMS$TAXP==23] <- 1150
+PUMS$TAXP2[PUMS$TAXP==24] <- 1250
+PUMS$TAXP2[PUMS$TAXP==25] <- 1350
+PUMS$TAXP2[PUMS$TAXP==26] <- 1450
+PUMS$TAXP2[PUMS$TAXP==27] <- 1550
+PUMS$TAXP2[PUMS$TAXP==28] <- 1650
+PUMS$TAXP2[PUMS$TAXP==29] <- 1750
+PUMS$TAXP2[PUMS$TAXP==30] <- 1850
+PUMS$TAXP2[PUMS$TAXP==31] <- 1950
+PUMS$TAXP2[PUMS$TAXP==32] <- 2050
+PUMS$TAXP2[PUMS$TAXP==33] <- 2150
+PUMS$TAXP2[PUMS$TAXP==34] <- 2250
+PUMS$TAXP2[PUMS$TAXP==35] <- 2350
+PUMS$TAXP2[PUMS$TAXP==36] <- 2450
+PUMS$TAXP2[PUMS$TAXP==37] <- 2550
+PUMS$TAXP2[PUMS$TAXP==38] <- 2650
+PUMS$TAXP2[PUMS$TAXP==39] <- 2750
+PUMS$TAXP2[PUMS$TAXP==40] <- 2850
+PUMS$TAXP2[PUMS$TAXP==41] <- 2950
+PUMS$TAXP2[PUMS$TAXP==42] <- 3050
+PUMS$TAXP2[PUMS$TAXP==43] <- 3150
+PUMS$TAXP2[PUMS$TAXP==44] <- 3250
+PUMS$TAXP2[PUMS$TAXP==45] <- 3350
+PUMS$TAXP2[PUMS$TAXP==46] <- 3450
+PUMS$TAXP2[PUMS$TAXP==47] <- 3550
+PUMS$TAXP2[PUMS$TAXP==48] <- 3650
+PUMS$TAXP2[PUMS$TAXP==49] <- 3750
+PUMS$TAXP2[PUMS$TAXP==50] <- 3850
+PUMS$TAXP2[PUMS$TAXP==51] <- 3950
+PUMS$TAXP2[PUMS$TAXP==52] <- 4050
+PUMS$TAXP2[PUMS$TAXP==53] <- 4150
+PUMS$TAXP2[PUMS$TAXP==54] <- 4250
+PUMS$TAXP2[PUMS$TAXP==55] <- 4350
+PUMS$TAXP2[PUMS$TAXP==56] <- 4450
+PUMS$TAXP2[PUMS$TAXP==57] <- 4550
+PUMS$TAXP2[PUMS$TAXP==58] <- 4650
+PUMS$TAXP2[PUMS$TAXP==59] <- 4750
+PUMS$TAXP2[PUMS$TAXP==60] <- 4850
+PUMS$TAXP2[PUMS$TAXP==61] <- 4950
+PUMS$TAXP2[PUMS$TAXP==62] <- 5250
+PUMS$TAXP2[PUMS$TAXP==63] <- 5750
+PUMS$TAXP2[PUMS$TAXP==64] <- 6500
+PUMS$TAXP2[PUMS$TAXP==65] <- 7500
+PUMS$TAXP2[PUMS$TAXP==66] <- 8500
+PUMS$TAXP2[PUMS$TAXP==67] <- 9500
+# category 68 is $10000+;
+# approximate this with a draw over the range of high values for taxes paid in CoreLogic
+samp_taxes <- CLdata$TAX.AMOUNT[CLdata$TAX.AMOUNT > 10000 & !is.na(CLdata$TAX.AMOUNT)]
+PUMS$TAXP2[PUMS$TAXP==68 & !is.na(PUMS$TAXP)] <- sample(x=samp_taxes,size=sum(PUMS$TAXP==68,na.rm=T))
+
+# ------------------------------------------------------------------------------------------------
+# get ACS marginals by block group (housholder income) from American FactFinder; 2013 5-year tables
+# estimate the mean and standard deviation of ln(income) by block group;
+# we will fit a lognormal distribution to income
+# ------------------------------------------------------------------------------------------------
+
+# ACS table: B19001 (household income)
+income_marginal <- read.csv("ACS_13_5YR_B19001_with_ann.csv",header=TRUE,skip=1)[,c(1:3,seq(6,37,by=2))]
+
+# income is binned; convert it into $$ by taking the center value of each bin
+for(i in 1:nrow(income_marginal)){ # loop over blockgroup
+  marginal_samples <- c( rep(5000,income_marginal[i,4]),
+                         rep(12500,income_marginal[i,5]),
+                         rep(17500,income_marginal[i,6]),
+                         rep(22500,income_marginal[i,7]),
+                         rep(27500,income_marginal[i,8]),
+                         rep(32500,income_marginal[i,9]),
+                         rep(37500,income_marginal[i,10]),
+                         rep(42500,income_marginal[i,11]),
+                         rep(47500,income_marginal[i,12]),
+                         rep(55000,income_marginal[i,13]),
+                         rep(65000,income_marginal[i,14]),
+                         rep(87500,income_marginal[i,15]),
+                         rep(112500,income_marginal[i,16]),
+                         rep(137500,income_marginal[i,17]),
+                         rep(175000,income_marginal[i,18]))
+  # the final bin is for $200,000+;
+  # approximate it with a draw over the range of high values of income from ACS PUMS data
+  samp_income <- PUMS$HINCP[PUMS$HINCP > 200000 & !is.na(PUMS$HINCP)]
+  marginal_samples <- c(marginal_samples,sample(x=samp_income,size=income_marginal[i,19]))
+
+  # compute the mean and sd of ln(income) for each block group
+  income_marginal$log_mean[i] <- mean(log(marginal_samples))
+  income_marginal$log_sd[i] <- sd(log(marginal_samples))
+}
+
+# join the ACS marginal information to CLdata by block group 
+blockgroups <- unique(CLdata$BlockGroup)
+ACS_marginals <- income_marginal %>% dplyr::select(BlockGroup=Id2,log_mean,log_sd)
+ACS_marginals$BlockGroup <- as.numeric(substr(ACS_marginals$BlockGroup,6,12))
+ACS_marginals <- ACS_marginals %>% filter(BlockGroup %in% CLdata$BlockGroup)
+CLdata <- merge(CLdata,ACS_marginals,by="BlockGroup")
+
+# save CLdata, PUMS, ACS_marginals data frames to use in inference
+save(CLdata,PUMS,ACS_marginals,file = "cleaned_CL_ACS.RData")
+
