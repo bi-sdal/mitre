38c728722f14d425739272988b474ac23ef932bd
Daniel Chen <Maveri2k@yahoo.com>
2018-01-17 18:45:21 +0000

    remove non-relevant q1 code/plot out

--- src/q1_file_analysis/03-plot_stuff.R
+++ src/q1_file_analysis/03-plot_stuff.R
@@ -40,35 +40,3 @@ ggplot(data = ct_size_plot, aes(x = group,
 dev.off()
 
 # File extensions and size ----
-
-## total number of bytes by file extension
-
-ext_size <- ldf %>%
-  group_by(fext) %>%
-  summarize(
-    count = n(),
-    total_size = sum(size)
-  ) %>%
-  arrange(total_size)
-
-ext_size$fext <- factor(ext_size$fext, levels = ext_size$fext[order(ext_size$total_size)])
-
-# View(ext_size)
-
-ggplot(data = ext_size, aes(x = fext, y = total_size)) +
-  geom_bar(stat = 'identity') +
-  #geom_hline(yintercept = 10e+9) + # 10 gb
-  #geom_hline(yintercept = 1e+9) +  # 1 gb
-  #geom_hline(yintercept = 5e+8) +  # 500 mb
-  #geom_hline(yintercept = 1e+6) +  # 1 mb
-  #scale_y_log10() +
-  coord_flip() +
-  theme_minimal()
-
-ggplot(data = ext_size, aes(x = fext, y = total_size)) + geom_boxplot() + coord_flip()
-
-# Overal decriptive calculations ----
-
-sum(ldf$size) / 1e+9 # number of gb of data
-
-max(ldf$size) / 1e+9
