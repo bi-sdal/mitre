197b750cc8f7207e5dd2e53750a2280f31d779b5
Ian Crandell <ian85@vbi.vt.edu>
2018-03-30 18:01:05 +0000

    more regex

--- src/ems_charts/domestic_violence_ems.R
+++ src/ems_charts/domestic_violence_ems.R
@@ -1,21 +1,23 @@
 library(stringr)
+library(jsonlite)
 
 ems_2013 <- list.files(path = '/home/sdal/projects/arl/arlington911/data/working/ems/EMSCharts/2013/Usable/', pattern = '*.txt', full.names = TRUE)
 
-ems_txt <- lapply(X = ems_2013, FUN = function(x){paste(readLines(x), collapse = ' ')})
+tmp = fromJSON("./data/arlington911/data/working/ems/EMSCharts/2013/fieldDump2013.json")
+
+ems_txt <- lapply(X = ems_2013[1:100], FUN = function(x){paste(readLines(x), collapse = ' ')})
 ems_txt_v <- unlist(ems_txt)
 
-# txt <- readLines('/home/sdal/projects/arl/arlington911/data/working/ems/EMSCharts/2013/Usable/newfile22286813.txt')
-# 
-# txt
-# 
-# txt_lower <- str_to_lower(txt)
-# 
-# txt1l <- paste(txt_lower, collapse = ' ')
-# 
-# txt1l
+# work with a subset for now
 
-assault_terms <- c('assault', 'battery', 'domestic violence', 'domestic', 'violence', 'abuse')
+emsSub = ems_txt_v
+
+# Do some cleaning
+
+emsSub = lapply(emsSub, function(x) gsub("\xa0", "", x))
+
+
+assault_terms <- c('assault', 'battery', 'domestic', 'violence', 'abuse')
 substance_terms <- c('drunk', 'drunken', 'intoxi', 'drug', 'substance', 'disorderly', 'alcohol')
 peep_terms <- c('child', 'elder', 'spouse', 'wife', 'husband')
 
@@ -23,14 +25,17 @@ all_terms <- c(assault_terms, substance_terms, peep_terms)
 
 reg_all <- sprintf('.{0,50}%s.{0,50}', all_terms)
 
-# reg_assault <- sprintf('.{0,50}%s.{0,50}', assault_terms)
-# reg_substance <- sprintf('.{0,50}%s.{0,50}', substance_terms)
-# reg_peep <- sprintf('.{0,50}%s.{0,50}', peep_terms)
+reg_assault <- sprintf('.{0,50}%s.{0,50}', assault_terms)
+reg_substance <- sprintf('.{0,50}%s.{0,50}', substance_terms)
+reg_peep <- sprintf('.{0,50}%s.{0,50}', peep_terms)
 
-assault_extracted <- lapply(ems_txt_v, FUN = str_extract_all, pattern = reg_assault)
+extractMatchWithContext <- lapply(emsSub, FUN = str_extract_all, pattern = reg_all)
+
+
+assault_extracted <- lapply(emsSub, FUN = str_extract_all, pattern = reg_assault)
   #str_extract_all(ems_txt_v, reg_assault)
 names(assault_extracted) <- assault_terms
-#assault_extracted
+assault_extracted
 
 substance_extracted <- str_extract_all(ems_txt_v, reg_substance)
 #names(substance_extracted) <- substance_terms
@@ -40,5 +45,27 @@ peep_extracted <- str_extract_all(ems_txt_v, reg_peep)
 #names(peep_extracted) <- peep_terms
 #peep_extracted
 
-m <- lapply(ems_txt_v[1:10], FUN = str_extract_all, pattern = all_terms)
-m
+# Extract GPS coords
+
+dispGpsRegex = "(?<=Disp\\.GPS:\\s{1,3})(-*\\d{2}\\.\\d{4,6},*-*\\d{2}\\.\\d{4,6})"
+refGpsRegex = "(?<=Ref\\.\\s{1,3}GPS:\\s{1,3})(-*\\d{2}\\.\\d{4,6},*-*\\d{2}\\.\\d{4,6})"
+destGpsRegex = "(?<=Dest\\.\\s{1,3}GPS:\\s{1,3})(-*\\d{2}\\.\\d{4,6},*-*\\d{2}\\.\\d{4,6})"
+str_extract(emsSub, dispGpsRegex)
+str_extract(emsSub, refGpsRegex)
+str_extract(emsSub, destGpsRegex)
+
+dateRegex = "(?<=Date:\\s{1,3})([a-zA-Z]{3,9}\\s*\\d{1,2},*\\s*\\d{4})"
+str_extract(emsSub, dateRegex)
+
+timeReceivedRegex = "(?<=Received:)(\\d{2}:\\d{2}:\\d{2})"
+str_extract(emsSub, timeReceivedRegex)
+
+# Experimental regex
+
+serviceLevelRegex = "(?<=Service Level:)(.*?)(?=\\s{5})"
+str_extract(emsSub, serviceLevelRegex)
+
+complaintRegex = "(?<=Category:).*?(?=\\){1,2})"
+str_extract(emsSub, complaintRegex)
+
+# Find patterns for 'supercategories', determine which records have those, assign blocks of text between supercategories to the leading category
