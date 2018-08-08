184fe1afab919aea9cc27235593a23321b951376
Ian Crandell <ian85@vbi.vt.edu>
2018-08-08 13:01:46 +0000

    nothig noteworthy

--- src/filterPoliceDataKevin.R
+++ src/filterPoliceDataKevin.R
@@ -10,7 +10,7 @@ dataDir = '/home/sdal/projects/arl/arlington911/data/original/police/aug082016/'
 dispatch = read.xls(sprintf("%scfs2015.xls", dataDir))
 dispatch = filter(dispatch, Original_Call == "DOME")
 reportNumbers = na.omit(dispatch$Report_No)
-
+head(dispatch)
 
 inper = read.xls(sprintf("%sinmast-inper62015.xls", dataDir))
 inper = filter(inper, Report_No %in% reportNumbers)
